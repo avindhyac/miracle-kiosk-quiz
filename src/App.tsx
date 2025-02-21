c54ea5a3845388a64c743fcce57eaa4b0fe5bc24
Avindhya Cabral <avindhya.cabral@gmail.com>
2025-02-22 03:35:00 +0530

    minor functionality changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import QuizFlow from './components/QuizFlow';
 import WelcomeScreen from './components/WelcomeScreen';
 
